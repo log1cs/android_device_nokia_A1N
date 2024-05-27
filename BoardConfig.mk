a86f2b044be852c9a778fb8d9a9cb6144d6dce3b
Tuan Anh <tuan73176@gmail.com>
2024-05-27 11:22:13 +0700

    A1N: Add manifest entries for Soter and IFAA
    * A1N supports Soter and IFAA.
    
    Change-Id: I2605b5e40633bf23af1f9dd90d13ee5ceed18d25

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/nokia/A1N
 # QCOM
 TARGET_BOARD_PLATFORM := msm8998
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Inherit from common device tree
 include device/nokia/msm8998-common/BoardConfigCommon.mk
 
@@ -20,9 +23,6 @@ TARGET_SUPPORT_HAL1 := false
 
 # Density
 TARGET_SCREEN_DENSITY := 520
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_A1N_defconfig
